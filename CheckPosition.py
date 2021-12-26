5d68218d3ea476817491d25ce9a9367b27ff012e
KDY\dea03 <dea0323@gmail.com>
2021-12-26 14:43:50 +0900

    add comment

--- CheckPosition.py
+++ CheckPosition.py
@@ -12,6 +12,9 @@ def main():
     {'e': 'ORDER_TRADE_UPDATE', 'T': 1640496696956, 'E': 1640496696964, 'o': {'s': 'ETHUSDT', 'c': 'android_ynd1I6QwEDqDkI7IU9II', 'S': 'SELL', 'o': 'MARKET', 'f': 'GTC', 'q': '0.001', 'p': '0', 'ap': '0', 'sp': '0', 'x': 'NEW', 'X': 'NEW', 'i': 8389765512848795674, 'l': '0', 'z': '0', 'L': '0', 'T': 1640496696956, 't': 0, 'b': '0', 'a': '0', 'm': False, 'R': True, 'wt': 'CONTRACT_PRICE', 'ot': 'MARKET', 'ps': 'BOTH', 'cp': False, 'rp': '0', 'pP': False, 'si': 0, 'ss': 0}}
     {'e': 'ACCOUNT_UPDATE', 'T': 1640496696956, 'E': 1640496696964, 'a': {'B': [{'a': 'USDT', 'wb': '4.86345961', 'cw': '4.86345961', 'bc': '0'}], 'P': [{'s': 'ETHUSDT', 'pa': '0', 'ep': '0.00000', 'cr': '-0.09337000', 'up': '0', 'mt': 'cross', 'iw': '0', 'ps': 'BOTH', 'ma': 'USDT'}, {'s': 'ETHUSDT', 'pa': '0', 'ep': '0.00000', 'cr': '-0.00090000', 'up': '0', 'mt': 'cross', 'iw': '0', 'ps': 'SHORT', 'ma': 'USDT'}], 'm': 'ORDER'}}
     {'e': 'ORDER_TRADE_UPDATE', 'T': 1640496696956, 'E': 1640496696964, 'o': {'s': 'ETHUSDT', 'c': 'android_ynd1I6QwEDqDkI7IU9II', 'S': 'SELL', 'o': 'MARKET', 'f': 'GTC', 'q': '0.001', 'p': '0', 'ap': '4025.18000', 'sp': '0', 'x': 'TRADE', 'X': 'FILLED', 'i': 8389765512848795674, 'l': '0.001', 'z': '0.001', 'L': '4025.18', 'n': '0.00161007', 'N': 'USDT', 'T': 1640496696956, 't': 1311468241, 'b': '0', 'a': '0', 'm': False, 'R': True, 'wt': 'CONTRACT_PRICE', 'ot': 'MARKET', 'ps': 'BOTH', 'cp': False, 'rp': '-0.01556000', 'pP': False, 'si': 0, 'ss': 0}}
+    size : 'pa': '0.002'
+    realized PNL : 'rp': '0.00220000'
+    Fee : 'n': '0.00161169'
     """
     def handle_socket_message(msg):
         #print(f"message type: {msg['e']}")
